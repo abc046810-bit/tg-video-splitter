9031ae0042b7f4cef8e50aca1cc3be635377c6bf
TheSk08 <abc046810@gmail.com>
2026-08-04 21:09:02 +0530

    Update states.py

--- states.py
+++ states.py
@@ -7,3 +7,6 @@ class BotState(IntEnum):
     SPLIT_ENTER_DURATION = auto()
     SPLIT_WAIT_VIDEO = auto()
     MERGE_COLLECT = auto()
+    TIMELINE_WAIT_VIDEO = auto()
+    TIMELINE_WAIT_BUTTON = auto()
+    TIMELINE_WAIT_RANGES = auto()
